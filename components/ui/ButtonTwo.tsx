88ea8c3abfdb4b5a52679d0da5092d8036b51385
samuel <samuelhope805@gmail.com>
2025-04-03 14:33:48 +0300

    first commit

--- components/ui/ButtonTwo.tsx
+++ components/ui/ButtonTwo.tsx
@@ -5,9 +5,11 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/utils/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors",
-  "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2",
-  "disabled:opacity-50 disabled:pointer-events-none",
+  [
+    "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors",
+    "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2",
+    "disabled:opacity-50 disabled:pointer-events-none",
+  ],
   {
     variants: {
       variant: {

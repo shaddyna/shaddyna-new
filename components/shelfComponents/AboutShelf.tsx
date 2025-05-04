8356525f2ab190515eeb28d50b8d14a216be4a5c
samuel <samuelhope805@gmail.com>
2025-05-04 10:55:38 +0300

    first commit

--- components/shelfComponents/AboutShelf.tsx
+++ components/shelfComponents/AboutShelf.tsx
@@ -17,9 +17,9 @@ export const AboutShelf: React.FC<AboutShelfProps> = ({ shelf }) => {
         <div className="mb-8">
           <h3 className="text-lg font-semibold text-[#0f1c47] mb-3">Shelf Rules</h3>
           <div className="bg-gray-50 p-4 rounded-md">
-            {shelf.rules.split('\n').map((rule, i) => (
+           {/*{shelf.rules.split('\n').map((rule, i) => (
               <p key={i} className="text-gray-700 mb-2">{rule}</p>
-            ))}
+            ))}*/}
           </div>
         </div>
 
@@ -37,11 +37,11 @@ export const AboutShelf: React.FC<AboutShelfProps> = ({ shelf }) => {
             <div>
               <p className="text-sm text-gray-500">Post Types</p>
               <div className="flex flex-wrap gap-2 mt-1">
-                {shelf.type?.map((type) => (
+                {/*{shelf.type?.map((type) => (
                   <span key={type} className="px-2 py-1 bg-gray-100 text-gray-700 rounded-full text-xs">
                     {type}
                   </span>
-                ))}
+                ))}*/}
               </div>
             </div>
             <div>
